ae8f3fafaa1c5b043c189341efc1092f9d9a00e6
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-09 22:09:35 +0900

    라우터 재적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Main } from "./main/Main";
-import { SubIndex } from "./sub/SubIndex";
+import { BuildingCanvas } from "./sub/building/Canvas";
+import { StartScreen } from "./sub/startScreen/ScreenIndex";
+import { VehicleCanvas } from "./sub/vehicleSimulation/Canvas";
 
 function App() {
   return (
-    <>
-      {/* <Main /> */}
-      <SubIndex />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<StartScreen />} />
+        <Route path="/exp/1" element={null} />
+        <Route path="/simulation/1" element={<VehicleCanvas />} />
+        <Route path="/etc/1" element={<BuildingCanvas />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
