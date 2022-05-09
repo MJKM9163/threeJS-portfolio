70b8b02be351b0ee9c9a01e64bee4c35e937dee1
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-09 22:09:09 +0900

    삭제

--- src/sub/Camera.js
+++
@@ -1,7 +0,0 @@
-import { useThree } from "@react-three/fiber";
-
-export const Camera = () => {
-  const { camera } = useThree();
-  camera.position.set(200, 600, 800);
-  camera.lookAt(0, 0, 0);
-};
--- src/sub/Light.js
+++
@@ -1,3 +0,0 @@
-export const Light = () => {
-  return <pointLight position={[0, 100, 0]} color={"white"} intensity={1} />;
-};
