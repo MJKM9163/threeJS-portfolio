de9de57d39fe1d4bc60bd792a889331a612a5792
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-09 22:10:53 +0900

    list 추가, 건물 초기화 상태 추가

--- src/stores/SubStore.js
+++ src/stores/SubStore.js
@@ -3,6 +3,13 @@ import { subscribeWithSelector } from "zustand/middleware";
 
 export const SubStore = create(
   subscribeWithSelector((set, get) => ({
+    screenList: {
+      exp: [["동산"]],
+      simulation: [["차량 브레이크 고장 시뮬레이션"]],
+      etc: [["미니 빌딩 세우기"]],
+    },
+
+    buildingDefault: false,
     buildingPos: [],
   }))
 );
