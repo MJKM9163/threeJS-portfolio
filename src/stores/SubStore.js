32068d7f1e4c55ec560658359027759e4b2f865e
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-11 21:14:53 +0900

    자동차 상태 추가

--- src/stores/SubStore.js
+++ src/stores/SubStore.js
@@ -11,5 +11,34 @@ export const SubStore = create(
 
     buildingDefault: false,
     buildingPos: [],
+
+    carSelect: "hyuneai",
+    carList: {
+      hyuneai: {
+        common: {
+          radius: 2,
+          width: 5.2,
+          height: -1.3,
+          front: 6.7,
+          back: -6.2,
+          steer: 0.5,
+          force: 5000,
+          maxBrake: 1e5,
+        },
+        option: {
+          directionLocal: [0, -1, 0],
+          suspensionStiffness: 10,
+          suspensionRestLength: 0.8,
+          maxSuspensionForce: 1e4,
+          maxSuspensionTravel: 0.8,
+          dampingRelaxation: 10,
+          dampingCompression: 4.4,
+          axleLocal: [-1, 0, 0],
+          useCustomSlidingRotationalSpeed: true,
+          customSlidingRotationalSpeed: 30,
+          frictionSlip: 5,
+        },
+      },
+    },
   }))
 );
