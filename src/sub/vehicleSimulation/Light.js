b6327d8924022f03d71f3b173fe5b0abd695d51b
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-11 21:15:39 +0900

    자동차 시뮬

--- src/sub/vehicleSimulation/Light.js
+++ src/sub/vehicleSimulation/Light.js
@@ -1,5 +1,5 @@
 export const Light = () => {
   return (
-    <pointLight position={[100, 800, 150]} color={"white"} intensity={1.5} />
+    <pointLight position={[100, 800, 150]} color={"white"} intensity={1} />
   );
 };
