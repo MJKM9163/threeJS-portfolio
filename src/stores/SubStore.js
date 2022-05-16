9f7c23921146ac9304bdd30309b7f396554e593a
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-16 22:03:16 +0900

    바람 시뮬레이션 링크 추가

--- src/stores/SubStore.js
+++ src/stores/SubStore.js
@@ -5,7 +5,7 @@ export const SubStore = create(
   subscribeWithSelector((set, get) => ({
     screenList: {
       exp: [["동산"]],
-      simulation: [["차량 브레이크 고장 시뮬레이션"], ["유리 시뮬레이션"]],
+      simulation: [["차량 브레이크 고장 시뮬레이션"], ["바람 시뮬레이션"]],
       etc: [["미니 빌딩 세우기"]],
     },
 
