41a6738e3138e177f315530d395a27f24ab8c52a
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-19 20:41:27 +0900

    바다 조명

--- src/sub/exp/ocean/Light.js
+++ src/sub/exp/ocean/Light.js
@@ -1,3 +1,31 @@
+import { useHelper } from "@react-three/drei";
+import { useRef } from "react";
+import { DirectionalLightHelper, PointLightHelper } from "three";
+
 export const Light = () => {
-  return <pointLight position={[0, 1000, 0]} color={"white"} intensity={1} />;
+  const pointLight = useRef();
+  useHelper(pointLight, PointLightHelper, 20.5, "lightblue");
+
+  return (
+    <>
+      <pointLight
+        ref={pointLight}
+        position={[0, 2000, 0]}
+        color={"white"}
+        intensity={1}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-camera-far={10000}
+        shadow-camera-near={0.1}
+        shadow-camera-left={-5000}
+        shadow-camera-right={5000}
+        shadow-camera-top={-5000}
+        shadow-camera-bottom={5000}
+        shadow-radius={1}
+        //shadow-bias={0.5}
+      />
+      <ambientLight intensity={0.1} />
+    </>
+  );
 };
