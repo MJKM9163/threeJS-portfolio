0e0839e1bb6ac60e169ab6b12869f2b3591f4984
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-19 20:42:17 +0900

    코드 순서 수정

--- src/sub/exp/ocean/OceanCanvas.js
+++ src/sub/exp/ocean/OceanCanvas.js
@@ -1,25 +1,35 @@
-import { Physics } from "@react-three/cannon";
-import { OrbitControls } from "@react-three/drei";
+import { Debug, Physics } from "@react-three/cannon";
+import { OrbitControls, softShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Light } from "./Light";
 import { Camera } from "./Camera";
 import { OceanIndex } from "./OceanIndex";
+import { Wall } from "./Wall";
 
+softShadows();
 export const OceanCanvas = () => {
   return (
     <Canvas
+      shadows
       camera={{
         fov: 60,
-        far: 50000,
-        near: 3,
+        far: 10000,
+        near: 0.5,
       }}>
-      <color attach="background" args={["#585858"]} />
-      <ambientLight intensity={0} />
-      {/* <Physics gravity={[0, -20, 0]} iterations={1}> */}
-      <OceanIndex />
-      {/* </Physics> */}
-      <Camera />
       <Light />
+      <color attach="background" args={["#585858"]} />
+
+      <Physics gravity={[0, 0, 0]} iterations={1}>
+        {/* <Debug scale={1.01} color="black"> */}
+        <OceanIndex />
+        {/* <Wall rotation={[0, 0, 0]} position={[0, 0, 1500]} />
+          <Wall rotation={[0, 0, 0]} position={[0, 0, -1500]} />
+          <Wall rotation={[0, Math.PI / 2, 0]} position={[1500, 0, 0]} />
+          <Wall rotation={[0, Math.PI / 2, 0]} position={[-1500, 0, 0]} /> */}
+        {/* </Debug> */}
+        <axesHelper args={[2000, 2000, 2000]} />
+      </Physics>
+      <Camera />
       <OrbitControls />
     </Canvas>
   );
