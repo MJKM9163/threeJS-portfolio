0a155b88e6b0cf19676146632e967fafecccb375
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-19 20:43:11 +0900

    해저 지형 스케일 수정

--- src/sub/exp/ocean/models/OceanFloor.js
+++ src/sub/exp/ocean/models/OceanFloor.js
@@ -13,73 +13,90 @@ export function OceanFloor({ ...props }) {
   const group = useRef();
   const { nodes, materials } = useGLTF("/oceans/oceanFloor/scene.gltf");
   return (
-    <group ref={group} {...props} dispose={null} receiveShadow>
+    <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
         <mesh
+          receiveShadow
           geometry={nodes.Object_2.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_3.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_4.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_5.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_6.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_7.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_8.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_9.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_10.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_11.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_12.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_13.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_14.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_15.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_16.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_17.geometry}
           material={materials.model_Material_u1_v1}
         />
         <mesh
+          receiveShadow
           geometry={nodes.Object_18.geometry}
           material={materials.model_Material_u1_v1}
         />
