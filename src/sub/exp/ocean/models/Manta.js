4bc4840e4585f374dfbde228be4a5a8f5709639c
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-25 04:18:48 +0900

    수정

--- src/sub/exp/ocean/models/Manta.js
+++ src/sub/exp/ocean/models/Manta.js
@@ -34,27 +34,6 @@ export default function Manta({ ...props }) {
 
   useEffect(() => {
     actions.Swimming.play();
-    // if (pRef.current) {
-    //   pRef.current.userData = {
-    //     name: "대왕쥐가오리(manta ray)",
-    //     habitat: "열대, 아열대",
-    //     characteristic: [
-    //       {
-    //         name: "거대한 가오리",
-    //         descript: "현존하는 가오리 중 가장 거대한 종",
-    //       },
-    //       {
-    //         name: "앞 지느러미",
-    //         descript: "앞 지느러미는 먹이를 입 주위로 모으는 역할을 한다.",
-    //       },
-    //       {
-    //         name: "여과 섭식자",
-    //         descript:
-    //           "여과 섭식자는 특화된 여과 구조를 가지고 있으며 물을 통과시켜 물속의 입자나 부유 물질을 걸러 먹는 포식자를 뜻한다.",
-    //       },
-    //     ],
-    //   };
-    // }
   });
 
   useEffect(() => {
