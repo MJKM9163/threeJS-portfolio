05413cb1c0eee396df3b4c1926659282d4c4bb24
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-26 23:25:24 +0900

    생물 컴포넌트 추가

--- src/sub/exp/ocean/OceanCanvas.js
+++ src/sub/exp/ocean/OceanCanvas.js
@@ -4,8 +4,9 @@ import { Canvas } from "@react-three/fiber";
 import { Light } from "./Light";
 import { Camera } from "./Camera";
 import { OceanIndex } from "./OceanIndex";
-import { Wall } from "./Wall";
+import { Background } from "./Background";
 import { UIIndex } from "./interface/UIIndex";
+import { Suspense } from "react";
 
 softShadows();
 export const OceanCanvas = () => {
@@ -23,16 +24,15 @@ export const OceanCanvas = () => {
         <color attach="background" args={["#585858"]} />
 
         <Physics gravity={[0, 0, 0]} iterations={1}>
-          {/* <Debug scale={1.01} color="black"> */}
-          <OceanIndex />
-          {/* <Wall rotation={[0, 0, 0]} position={[0, 0, 1500]} />
-          <Wall rotation={[0, 0, 0]} position={[0, 0, -1500]} />
-          <Wall rotation={[0, Math.PI / 2, 0]} position={[1500, 0, 0]} />
-          <Wall rotation={[0, Math.PI / 2, 0]} position={[-1500, 0, 0]} /> */}
-          {/* </Debug> */}
+          <Suspense fallback={null}>
+            {/* <Debug scale={1.01} color="black"> */}
+            <OceanIndex />
+            {/* </Debug> */}
+            <Camera />
+            <Background />
+          </Suspense>
           <axesHelper args={[2000, 2000, 2000]} />
         </Physics>
-        <Camera />
       </Canvas>
     </>
   );
