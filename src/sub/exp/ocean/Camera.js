37d75ab13de264a70910d85f84bfaefc44017f17
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-26 23:24:38 +0900

    시작 카메라 무빙 추가

--- src/sub/exp/ocean/Camera.js
+++ src/sub/exp/ocean/Camera.js
@@ -1,15 +1,31 @@
 import { OrbitControls } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 import { OceanStore } from "./OceanStore";
-
+let cameraPos = [0, 1800, 0];
+let check = false;
 export const Camera = () => {
   const { camera } = useThree();
   const cameraTarget = OceanStore((state) => state.oceanCameraTarget);
 
   if (!cameraTarget) {
-    camera.position.set(800, 1600, 1600);
+    camera.position.set(0, 1800, 0);
+    // camera.position.set(600, 1300, 1300);
     camera.lookAt(0, 0, 0);
   }
 
-  return <OrbitControls enablePan={false} />;
+  useFrame(() => {
+    if (!check) {
+      camera.position.set(
+        (cameraPos[0] += Math.floor((600 - cameraPos[0]) / 50)),
+        (cameraPos[1] -= Math.floor((cameraPos[1] - 1300) / 50)),
+        (cameraPos[2] += Math.floor((1300 - cameraPos[2]) / 50))
+      );
+      if (camera.position.y <= 1350) {
+        check = true;
+      }
+    }
+  });
+
+  return <OrbitControls enableZoom={false} enablePan={false} />;
 };
