7469c941fe80c90e0499691edc1d7d7d1cedc69d
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-05-27 22:08:23 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { BuildingCanvas } from "./sub/etc/building/Canvas";
+import { ChemicalCanvas } from "./sub/exp/chemical/ChemicalCanvas";
 import { OceanCanvas } from "./sub/exp/ocean/OceanCanvas";
 import { VehicleCanvas } from "./sub/simulations/vehicle/VehicleCanvas";
 import { StartScreen } from "./sub/startScreen/ScreenIndex";
@@ -14,6 +15,7 @@ function App() {
         <Route path="/exp/1" element={<OceanCanvas />} />
         <Route path="/simulation/1" element={<VehicleCanvas />} />
         <Route path="/etc/1" element={<BuildingCanvas />} />
+        <Route path="/etc/2" element={<ChemicalCanvas />} />
       </Routes>
     </BrowserRouter>
   );
